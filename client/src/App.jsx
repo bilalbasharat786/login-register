239c8df3cf463d450f24d709c16d7f0b2f133410
Bilal Basharat <hassanrohan366@gmail.com>
2025-09-28 15:49:01 +0500

    Initial commit for fullstack login-register project

---
+++ client/src/App.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react"; // useState hook import
+
+function App() {
+  // students ka array banaya
+  const [students, setStudents] = useState([
+    { id: 1, name: "Ali" },
+    { id: 2, name: "Sara" },
+    { id: 3, name: "Bilal" },
+  ]);
+
+  return (
+    <div>
+      <h1>Lists and Keys Example</h1>
+
+      {/* ---------- LIST RENDERING with map() ---------- */}
+      <ul>
+        {students.map((student) => (
+          
+          <li key={student.id}>{student.name}</li>
+        ))}
+      </ul>
+
+      {/* ---------- ADDING NEW ITEM (Dynamic list) ---------- */}
+      
+    </div>
+  );
+}
+
+export default App;
+
+
+
