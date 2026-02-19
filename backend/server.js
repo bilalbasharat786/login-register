32aa31ccd007f5faca3205e66eb0d3277ec1e734
sfmtechnicals-art <sfmtechnicals@gmail.com>
2026-02-19 16:38:42 +0500

    api working

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,10 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.get('/', (req, res) => {
+  res.send('API WORKING');
+});
+
 app.use('/api/auth', authRoutes);
 app.use('/api/protected', protectedRoutes);
 
